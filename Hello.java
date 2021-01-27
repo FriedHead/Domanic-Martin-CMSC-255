7aa8a8dd0d7e32f266e514bb2f165bfe127976fe
FriedHead <78099054+FriedHead@users.noreply.github.com>
2021-01-27 11:52:41 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/**************************
+ *Lab 1 
+ **************************
+ * Domanic Martin
+ * January 27th 2021
+ * CMSC 255 003
+ *************************/
+
+public class Hello {
+	public static void main(String[] args) {
+		
+		System.out.println("Domanic Martin");
+		System.out.println("Lab 1");
+		System.out.println("Have Java installed and creating and running a Java Program");
+		System.out.println("January 27th 2021");
+		System.out.println("CMSC 255 Section 003");
+		System.out.println();
+	}
+}
